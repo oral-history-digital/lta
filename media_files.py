a378eeae83d4776945b112e843441fbfb0b9143d
Marc Altmann <marc.altmann@cedis.fu-berlin.de>
2022-05-16 18:57:41 +0200

    Fix media_files.py

--- media_files.py
+++ media_files.py
@@ -1,34 +1,39 @@
 import os
 import re
 
-def filter_media_files(filename):
+def get_file_extension(filename):
     pair = os.path.splitext(filename)
     ext = pair[1].lower()
+    return ext
+
+def filter_media_files(filename):
+    ext = get_file_extension(filename)
     return ext in ['.m2ts', '.mp4', '.avi']
 
 def filter_transcript_files(filename):
-    pair = os.path.splitext(filename)
-    ext = pair[1].lower()
+    ext = get_file_extension(filename)
     return ext in ['.ods', '.pdf']
+
+def filter_blacklisted_files(filename):
+    ext = get_file_extension(filename)
+    return ext not in ['.DS_Store', '.sha256', '.xml']
 
 def check_directory_integrity(path):
     files = list(os.listdir(path))
-    if '.DS_Store' in files:
-        files.remove('.DS_Store')
+    files_after_blacklist = list(filter(filter_blacklisted_files, files))
 
-    first_file = files[0]
+    first_file = files_after_blacklist[0]
     m = re.search('_(\d{2})_', first_file)
     first_match = m.group(1)
 
     num_parts = int(first_match)
 
-    media_files = list(filter(filter_media_files, list(files)))
-    transcript_files = list(filter(filter_transcript_files, list(files)))
-
+    media_files = list(filter(filter_media_files, files_after_blacklist))
+    transcript_files = list(filter(filter_transcript_files, files_after_blacklist))
     media_files.sort()
     transcript_files.sort()
 
-    if (len(files) != num_parts * 2):
+    if (len(files_after_blacklist) != num_parts * 2):
         raise ValueError('Wrong number of files.')
 
     if (len(media_files) != num_parts):
