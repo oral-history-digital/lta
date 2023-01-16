a3fcd8b7644158a6f318a5b857e140b6f68d2eb5
Marc Altmann <marcaltmann@posteo.de>
2023-01-16 10:29:26 +0100

    Update project structure again

---
+++ setup.py
@@ -0,0 +1,25 @@
+"""Minimal setup file for lta project."""
+
+from setuptools import setup, find_packages
+
+setup(
+    name='lta',
+    version='0.1.0',
+    license='GNU GPLv3',
+    description='OHD long term archiving tool',
+
+    author='Marc Altmann',
+    author_email='marc.altmann@cedis.fu-berlin.de',
+    url='',
+
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+
+    install_requires=['click==7.1.2', 'tinydb==3.15.1', 'six'],
+
+    entry_points={
+        'console_scripts': [
+            'lta = lta.cli:lta_cli',
+        ]
+    },
+)
---
+++ src/lta/__init__.py
@@ -0,0 +1,7 @@
+"""OHD long term archiving tool."""
+
+from .api import (
+
+)
+
+__version__ = '0.1.0'
